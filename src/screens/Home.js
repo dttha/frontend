18f5745fc48e45ab96f1f29b66fc9f7ccc447ccc
đttha <dothithuha140@gmail.com>
2022-08-07 16:26:25 +0700

    manage state

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,28 +1,67 @@
-import React from "react"
+import React, { useEffect, useReducer, useState } from "react"
 import { Link } from "react-router-dom"
-import data from "../data"
+import axios from 'axios'
+import logger from 'use-reducer-logger'
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCh_REQUEST':
+            return { ...state, loading: true };
+        case 'FETCH_SUCCESS':
+            return { ...state, products: action.payload, loading: false };
+        case 'FETCH_FAIL':
+            return { ...state, loading: false, error: action.payload };
+        default:
+            return state;
+    }
+}
 
 export default function Home() {
+    const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+        products: [],
+        loading: true,
+        error: '',
+    })
+    // const [products, setProducts] = useState([])
+    useEffect(() => {
+        fetchData()
+    }, [])
+    const fetchData = async () => {
+        dispatch({ type: 'FETCH_REQUEST' })
+        try {
+            const result = await axios.get('api/products')
+            dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
+        } catch (err) {
+            dispatch({ type: 'FETCH_FAIL', payload: err.message })
+        }
+        // setProducts(result.data)
+    }
     return <div>
         <h1>Sách được yêu thích</h1>
         <div className="products">
-            {data.products.map((product) => (
-                <div className="product" key={product.slug}>
-                    <Link to={`/product/${product.slug}`}>
-                        <img className="image-cover" src={product.image} alt={product.name} />
-                    </Link>
-                    <div className="product-info">
+            {loading ? (
+                <div>Loading...</div>
+            ) : error ? (
+                <div>{error}</div>
+            ) : (
+                products.map((product) => (
+                    <div className="product" key={product.slug}>
                         <Link to={`/product/${product.slug}`}>
-                            <p>
-                                {product.name}
-                            </p>
+                            <img className="image-cover" src={product.image} alt={product.name} />
                         </Link>
-                        <p>
-                            <strong>{product.price}đ</strong>
-                        </p>
-                        <button>Thêm vào giỏ hàng</button>
-                    </div>
-                </div>)
+                        <div className="product-info">
+                            <Link to={`/product/${product.slug}`}>
+                                <p>
+                                    {product.name}
+                                </p>
+                            </Link>
+                            <p>
+                                <strong>{product.price}đ</strong>
+                            </p>
+                            <button>Thêm vào giỏ hàng</button>
+                        </div>
+                    </div>)
+                        )
             )}
         </div>
     </div>
